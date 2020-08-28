ceed0c3e88b93dba9fae0254f58e334c9cc02e04
Barnaba <Denek20@gmail.com>
2020-08-28 16:42:22 +0200

    add rel nooper noreferrer atribute

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -6,9 +6,9 @@ export default function Aside(props) {
 
   return (
     <aside className={cssClasses.join(' ')}>
-      <a href="https://github.com/DenekD" target="_blank"><i class="fab fa-github"></i></a>
-      <a href="https://www.facebook.com/tomasz.denkiewicz.1" target="_blank"><i class="fab fa-facebook-f"></i></a>
-      <a href="https://www.linkedin.com/in/tomasz-denkiewicz-70700276/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+      <a href="https://github.com/DenekD" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+      <a href="https://www.facebook.com/tomasz.denkiewicz.1" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+      <a href="https://www.linkedin.com/in/tomasz-denkiewicz-70700276/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
     </aside>
   )
 }
